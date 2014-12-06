42d02ba3fef83328609cfdf1755d2b8122738882
busakorn <s7070062@kmitl.ac.th>
2014-12-06 21:09:37 +0700

    edit
    edit in calculate and about

--- test.py
+++ test.py
@@ -77,6 +77,14 @@ class Calculate():
         label_you = Label(self.master, text='Your BMI is : ', font=("Helvetica", 15), background= '#ffffff')
         label_you.place(x=10, y=270)
         label_bmi.place(x=140, y=265)
+
+        #Menu
+        f_menu = Button(self.master, text = 'Menu', bg = '#ffffff', command = self.menu, font=("Helvetica", 15), relief=RIDGE)
+        f_menu.place(x=60, y=320, width = 190)
+
+        #Exit
+        b_menu3 = Button(self.master, text = 'Exit', bg = '#ffffff', command = self.exits, font=("Helvetica", 15), relief=RIDGE)
+        b_menu3.place(x=270, y=320, width = 190)
         
         
         self.master.mainloop()
@@ -88,12 +96,61 @@ class Calculate():
         bmi = weight/((height/100.0)**2)
         self.update.set('%.2f' % bmi)
 
+    def menu(self):
+        self.master.destroy()
+        Menu()
+
+    def exits(self):
+        self.master.destroy()
+
         
 class About():
     def __init__(self):
-        self.maste = Tk()
-        self.maste.geometry("500x400+500+100")
-        self.maste.title("Body mass index")
-        self.maste.resizable(width=FALSE, height=FALSE)
+        self.master = Tk()
+        self.master.geometry("500x400+500+100")
+        self.master.title("Body mass index")
+        self.master.resizable(width=FALSE, height=FALSE)
+
+        #Background Color
+        self.master.configure(bg = '#ffffff')
+
+        #about
+        text_a = Message(self.master, text="\n\tThe body mass index (BMI), or Quetelet index, is a measure "
+                         "of relative weight based on an individual's mass and height.\n\n"
+                         "\tA frequent use of the BMI is to assess how much an individual's body weight "
+                         "departs from what is normal or desirable for a person of his or her height.\n"
+                         "The weight excess or deficiency may, in part, be accounted for by body fat \n"
+                         "(adipose tissue) although other factors such as muscularity also affect BMI "
+                         "significantly (see discussion below and overweight). The WHO regards a BMI of "
+                         "less than 18.5 as underweight and may indicate malnutrition, an eating disorder, "
+                         "or other health problems, while a BMI greater than 25 is considered overweight and "
+                         "above 30 is considered obese. These ranges of BMI values are valid only as statistical categories\n\n"
+                         "\tless than 15 = Very severely underweight\n"
+                         "\tfrom 15.0 to 16.0 = Severely underweight\n"
+                         "\tfrom 16.0 to 18.5 = Underweight\n"
+                         "\tfrom 18.5 to 25 = Normal (healthy weight)\n"
+                         "\tfrom 25 to 30 = Overweight\n"
+                         "\tfrom 30 to 35 = Obese Class I (Moderately obese)\n"
+                         "\tfrom 35 to 40 = Obese Class II (Severely obese)\n"
+                         "\tover 40 = Obese Class III (Very severely obese)", width=450, background = '#ffffff')
+        text_a.pack()
+
+        #Menu
+        f_menu = Button(self.master, text = 'Menu', bg = '#ffffff', command = self.menu, font=("Helvetica", 15), relief=RIDGE)
+        f_menu.place(x=60, y=340, width = 190)
+
+        #Exit
+        b_menu3 = Button(self.master, text = 'Exit', bg = '#ffffff', command = self.exits, font=("Helvetica", 15), relief=RIDGE)
+        b_menu3.place(x=270, y=340, width = 190)
+
+        self.master.mainloop()
+
+    def menu(self):
+        self.master.destroy()
+        Menu()
+
+    def exits(self):
+        self.master.destroy()
+
         
 Menu()  

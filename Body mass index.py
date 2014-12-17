07a4893c3cfa4ab03f0fe99b28fd7180c0586e54
Thanoo <nui_pk48@hotmai.com>
2014-12-17 16:44:50 +0700

    Project done!

--- Body mass index.py
+++ Body mass index.py
@@ -1,6 +1,5 @@
-# -*- coding: utf-8 -*-
 '''
-Body mass index v.2
+Body mass index
 NUI & TUKTA
 '''
 from Tkinter import *
@@ -10,9 +9,10 @@ class Menu():
       def __init__(self):
             '''Main'''
             self.master = Tk()
-            self.master.geometry("500x400+500+100")
+            self.master.geometry("500x400+500+150")
             self.master.title("Body mass index")
             self.master.configure(bg = '#8fc9ff')
+            self.master.wm_iconbitmap('image/icon.ico')
             self.master.resizable(width=FALSE, height=FALSE)
             
             #Logo
@@ -35,35 +35,39 @@ class Menu():
         
             self.master.mainloop()
       def calculate(self):
+            '''Go to calculate'''
             self.master.destroy()
             Calculate()
       def about(self):
+            '''Go to about'''
             self.master.destroy()
             About()
       def exits(self):
+            '''Exit program'''
             self.master.destroy()
 
 class Calculate():
       def __init__(self):
             self.master = Tk()
-            self.master.geometry("670x600+400+70")
+            self.master.geometry("650x590+400+70")
             self.master.title("Body mass index ~ Calculate Your BMI")
             self.master.configure(bg = '#8fc9ff')
+            self.master.wm_iconbitmap('image/icon.ico')
             self.master.resizable(width=FALSE, height=FALSE)
             #Logo
             logo2 = PhotoImage(file = "image/Logo2.gif")
             logo = Label(self.master, image = logo2, bg = '#8fc9ff')
-            logo.place(x=150, y=0)
+            logo.place(x=150, y=10)
 
             #Back to menu
             back_pic = PhotoImage(file = "image/menu.gif")
             b_back = Button(self.master, image = back_pic, command = self.back, relief = FLAT)
-            b_back.place(x=35, y=550, width=103, height=33)
+            b_back.place(x=70, y=540, width=103, height=33)
 
             #Exit
             exit_pic = PhotoImage(file = "image/exit1.gif")
             b_exit = Button(self.master, image = exit_pic, command = self.exits, relief = FLAT)
-            b_exit.place(x=135, y=550, width=103, height=33)
+            b_exit.place(x=185, y=540, width=103, height=33)
 
             #backgound Calculator
             back = PhotoImage(file = "image/back.gif")
@@ -82,14 +86,17 @@ class Calculate():
             self.standard()
 
             #Result BMI label
-            self.update = StringVar()
-            label_bmi = Label(self.master, text = '0', fg = 'white', font = ("Helvetica", 17), bg = '#8fc9ff', textvariable = self.update)
-            label_bmi.place(x=507, y=120)
-            self.label_zero = Label(self.master, text = '0', fg = 'white', font = ("Helvetica", 17), bg = '#8fc9ff')
-            self.label_zero.place(x=507, y=120)
+            back2 = PhotoImage(file = "image/back2.gif")
+            Label(self.master, image = back2, fg = 'white', background = '#8fc9ff').place(x=355, y=118)
             result = PhotoImage(file = "image/result.gif")
-            label_you = Label(self.master, image = result, fg = 'white', background = '#8fc9ff')
-            label_you.place(x=350, y=110)
+            label_you = Label(self.master, image = result, fg = 'white', background = '#6594c0')
+            label_you.place(x=375, y=138)
+            self.update = StringVar()
+            label_bmi = Label(self.master, text = '0', fg = 'white', font = ("Helvetica", 17), bg = '#6594c0', textvariable = self.update)
+            label_bmi.place(x=517, y=145)
+            self.label_zero = Label(self.master, text = '0', fg = 'white', font = ("Helvetica", 17), bg = '#6594c0')
+            self.label_zero.place(x=517, y=145)
+            
 
             #BMI Table
             table = PhotoImage(file = "image/bmi_table.gif")
@@ -103,16 +110,16 @@ class Calculate():
 
             #BMI Picture
             pic = PhotoImage(file = "image/normal.gif")
-            Label(self.master, image = pic, bg = '#8fc9ff').place(x=375, y=180)
+            Label(self.master, image = pic, bg = '#6594c0').place(x=400, y=180)
             self.pic1 = PhotoImage(file = "image/under.gif")
             self.pic2 = PhotoImage(file = "image/normal.gif")
             self.pic3 = PhotoImage(file = "image/risk.gif")
             self.pic4 = PhotoImage(file = "image/moder.gif")
             self.pic5 = PhotoImage(file = "image/sev.gif")
-
             self.master.mainloop()
 
       def metric(self):
+            '''metric input frame'''
             self.frame.destroy()
             self.frame = Frame(self.master, width = 300, height = 130, bg = '#6893ba')
             self.frame.place(x=30, y=150)
@@ -131,10 +138,11 @@ class Calculate():
             self.height_entry.place(x=65, y=54)
             self.cal_pic = PhotoImage(file = "image/calculate.gif")
             cal = Button(self.frame, image = self.cal_pic, command = self.cal_metric, relief = FLAT)
-            cal.place(x=80, y=90, width=103, height=33)
+            cal.place(x=100, y=95, width=103, height=33)
 
 
       def standard(self):
+            '''standard input frame'''
             self.frame.destroy()
             self.frame = Frame(self.master, width = 300, height = 130, bg = '#6893ba')
             self.frame.place(x=30, y=150)
@@ -156,9 +164,10 @@ class Calculate():
             self.height_in_entry.place(x = 137, y = 54)
             self.cal_pic = PhotoImage(file = "image/calculate.gif")
             cal = Button(self.frame, image = self.cal_pic, command = self.cal_standard, relief = FLAT)
-            cal.place(x=80, y=90, width=103, height=33)
+            cal.place(x=100, y=95, width=103, height=33)
       
       def cal_metric(self):
+            '''Calculate metric mode'''
             try:
                   weight = float(self.weight_entry.get())
                   height = float(self.height_entry.get())
@@ -170,6 +179,7 @@ class Calculate():
                   tkMessageBox.showerror('Error!', 'Please insert integer or float only')
 
       def cal_standard(self):
+            '''Calculate standard mode'''
             try:
                   weight = float(self.weight_entry.get())
                   height_in = (float(self.height_ft_entry.get())*12) + float(self.height_in_entry.get())
@@ -181,73 +191,73 @@ class Calculate():
                   tkMessageBox.showerror('Error!', 'Please insert integer or float only')
                   
       def bmi_table(self):
+            '''All table and Picture'''
             self.table.place_forget()
             if self.bmi < 18.5:
                   table = Label(self.master, image = self.table1, bg = '#8fc9ff')
+                  pic = Label(self.master, image = self.pic1, bg = '#6594c0')
                   table.place(x=30, y=300)
-                  pic = Label(self.master, image = self.pic1, bg = '#8fc9ff')
-                  pic.place(x=375, y=180)
+                  pic.place(x=400, y=180)
             elif self.bmi >= 18.5 and self.bmi < 23:
                   table = Label(self.master, image = self.table2, bg = '#8fc9ff')
+                  pic = Label(self.master, image = self.pic2, bg = '#6594c0')
                   table.place(x=30, y=300)
-                  pic = Label(self.master, image = self.pic2, bg = '#8fc9ff')
-                  pic.place(x=375, y=180)
+                  pic.place(x=400, y=180)
             elif self.bmi >= 23 and self.bmi < 25:
                   table = Label(self.master, image = self.table3, bg = '#8fc9ff')
+                  pic = Label(self.master, image = self.pic3, bg = '#6594c0')
                   table.place(x=30, y=300)
-                  pic = Label(self.master, image = self.pic3, bg = '#8fc9ff')
-                  pic.place(x=375, y=180)
+                  pic.place(x=400, y=180)
             elif self.bmi >= 25 and self.bmi < 30:
                   table = Label(self.master, image = self.table4, bg = '#8fc9ff')
+                  pic = Label(self.master, image = self.pic4, bg = '#6594c0')
                   table.place(x=30, y=300)
-                  pic = Label(self.master, image = self.pic4, bg = '#8fc9ff')
-                  pic.place(x=375, y=180)
+                  pic.place(x=400, y=180)
             else:
                   table = Label(self.master, image = self.table5, bg = '#8fc9ff')
+                  pic = Label(self.master, image = self.pic5, bg = '#6594c0')
                   table.place(x=30, y=300)
-                  pic = Label(self.master, image = self.pic5, bg = '#8fc9ff')
-                  pic.place(x=375, y=180)
+                  pic.place(x=400, y=180)
 
  
       def back(self):
+            '''Go to menu'''
             self.master.destroy()
             Menu()
 
       def exits(self):
+            '''Exit program'''
             self.master.destroy()
             
 class About():
       def __init__(self):
             self.master = Tk()
-            self.master.geometry("500x400+500+100")
+            self.master.geometry("500x400+500+150")
             self.master.title("Body mass index")
+            self.master.wm_iconbitmap('image/icon.ico')
             self.master.resizable(width = FALSE, height = FALSE)
 
-            #Background Color
-            self.master.configure(bg = '#b0e0e6')
-
             #about
-            text_a = Message(self.master, text="\nProvider", width=450, font = ("Helvetica", 20), background = '#b0e0e6')
-            text_b = Message(self.master, text="\nThanoo Chomyad 57070051"
-                            "\nBusakorn Yuangngoen 57070062", width=450, font = ("Helvetica", 15), background = '#b0e0e6')
-            text_a.pack()
-            text_b.pack()
+            page = PhotoImage(file = "image/about.gif")
+            text_a = Label(self.master, image = page, bg = '#8fc9ff')
+            text_a.place(x=0, y=0)
 
             #Menu
-            f_menu = Button(self.master, text = 'Menu', bg = '#0061ff', command = self.menu, font = ("Helvetica", 15), relief = RIDGE)
-            f_menu.place(x=60, y=340, width=190)
+            menu_pic = PhotoImage(file = "image/menu3.gif")
+            f_menu = Button(self.master, image = menu_pic, bg = '#8fc9ff', command = self.menu, relief = FLAT)
+            f_menu.place(x=20, y=340, width=230, height=40)
 
             #Exit
-            b_menu3 = Button(self.master, text = 'Exit', bg = '#ff0000', command = self.exits, font = ("Helvetica", 15), relief = RIDGE)
-            b_menu3.place(x=270, y=340, width=190)
+            exit_pic = PhotoImage(file = "image/exit.gif")
+            b_menu3 = Button(self.master, image = exit_pic, bg = '#8fc9ff', command = self.exits, relief = FLAT)
+            b_menu3.place(x=255, y=340, width=230, height=40)
 
             self.master.mainloop()
-
       def menu(self):
+            '''Go to menu'''
             self.master.destroy()
             Menu()
-
       def exits(self):
+            '''Exit program'''
             self.master.destroy()
-        
-Menu()  
+Menu()
